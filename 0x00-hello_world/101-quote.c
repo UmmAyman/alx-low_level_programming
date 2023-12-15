7f16b93a84646af8a81dfdd7fe029ab1810c14d6
UmmAyman <halimattemitope03@gmail.com>
2023-12-15 12:53:03 -0800

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A program that prints a line to the standard error
@@ -5,6 +6,6 @@
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",);
 return (1);
 }
